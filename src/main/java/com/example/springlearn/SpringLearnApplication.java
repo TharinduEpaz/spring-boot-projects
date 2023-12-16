9983e6b1c984295169571f3177de0736c380d150
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-12-16 16:47:39 +0530

    added the necessarry classes and spring structure

--- src/main/java/com/example/springlearn/SpringLearnApplication.java
+++ src/main/java/com/example/springlearn/SpringLearnApplication.java
@@ -5,17 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
 public class SpringLearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
     }
-
-    @GetMapping
-    public String hello(){
-        return "hello";
-    }
-
 }
